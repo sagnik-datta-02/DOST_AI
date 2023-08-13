a36969d13003fead3d571c48ba95a6b4aeb248c7
Sagnik Datta <127123265+sagnik-datta-02@users.noreply.github.com>
2023-08-13 07:12:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import { Paper, Typography, TextField, Button, List, ListItem, Container } from '@mui/material';
+import { Paper, Typography, TextField, Button, List, ListItem, Container, ArrowBack , Grid} from '@mui/material';
 import ChatMessage from './components/ChatMessage';
+
 
 function ChatBotApp() {
   const [questions] = useState([
-    'How are you feeling today?',
-    'What is your favorite color?',
-    'Tell me about your day.',
+    //'How are you feeling today?',
+    //'What is your favorite color?',
+    //'Tell me about your day.',
+    "Are you feeling happy or sad. How often u feel so?",
+"What do you think about your future?"
+//"Do you feel like a failure?",
+//"How satisfied are you with things you used to enjoy?",
+//"How often do you feel guilty?"
+//,"Do you feel like you're being punished?"
+,"How do you feel about yourself?"
+//,"Do you blame yourself for things?"
+//,"Do you have thoughts of killing yourself?"
+//,"How often do you cry?"
+,"How are you behaving with others usually nowadays?"
+//,"Have you lost interest in other people?"
+,"How well can you make decisions?"
+,"What do you feel about your appearance?"
+//,"How well can you work compared to before?"
+//,"How well are you sleeping?"
+//,"Do you feel more tired than usual?"
+//,"How's your appetite compared to usual?"
+//,"Have you lost weight recently?"
+,"How worried are you about anything in last few days?"
+//,"Have you lost interest in sex?"
     // Add more questions here
   ]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -86,52 +108,76 @@ function ChatBotApp() {
     return total / values.length;
   };
 
-  
+
   return (
     <Container maxWidth="md" style={{ marginTop: '40px' }}>
-      <Paper elevation={3} style={{ padding: '20px', height: '60vh', overflowY: 'auto' }}>
-        <Typography variant="h4" align="center" gutterBottom>
-          MedAid Dost
-        </Typography>
+  <Paper elevation={3} style={{ padding: '20px', height: '80vh', overflowY: 'auto' }}>
+    {/* Header */}
+    <Grid container justifyContent="space-between" alignItems="center" marginBottom="20px">
+      <Typography variant="h4" align="center">
+        MedAid Dost
+      </Typography>
+      {currentQuestionIndex === questions.length && (
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={() => window.location.reload()} // Reload the page to go back to the "home"
+        >
+          Go to Home
+        </Button>
+      )}
+    </Grid>
 
-        <List sx={{ overflowY: 'auto', maxHeight: '300px', marginBottom: '15px', borderRadius: '10px', padding: '10px', backgroundColor: '#f4f4f4' }}>
-          {messageHistory.map((messageData, index) => (
-            <ListItem key={index} sx={{ marginBottom: '10px', borderRadius: '10px', padding: '10px', display: 'flex', justifyContent: messageData.isUser ? 'flex-end' : 'flex-start' }}>
-              <ChatMessage message={messageData.message} isUser={messageData.isUser} />
-            </ListItem>
-          ))}
-        </List>
+    {/* Message List */}
+    <List sx={{ overflowY: 'auto', maxHeight: '60vh', marginBottom: '15px', borderRadius: '10px', padding: '10px', backgroundColor: '#f4f4f4' }}>
+      {messageHistory.map((messageData, index) => (
+        <ListItem
+          key={index}
+          sx={{
+            marginBottom: '10px',
+            borderRadius: '10px',
+            padding: '10px',
+            display: 'flex',
+            justifyContent: messageData.isUser ? 'flex-end' : 'flex-start',
+          }}
+        >
+          <ChatMessage message={messageData.message} isUser={messageData.isUser} />
+        </ListItem>
+      ))}
+    </List>
 
-        {currentQuestionIndex < questions.length && (
-          <div>
-            <TextField
-              label="Type your answer..."
-              variant="outlined"
-              fullWidth
-              value={userInput}
-              onChange={(event) => setUserInput(event.target.value)}
-              sx={{ marginTop: '10px' }}
-            />
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleUserResponse}
-              sx={{ marginTop: '10px' }}
-            >
-              Submit
-            </Button>
-          </div>
-        )}
+    {/* User Input */}
+    {currentQuestionIndex < questions.length && (
+      <div>
+        <TextField
+          label="Type your answer..."
+          variant="outlined"
+          fullWidth
+          value={userInput}
+          onChange={(event) => setUserInput(event.target.value)}
+          sx={{ marginTop: '10px' }}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleUserResponse}
+          sx={{ marginTop: '10px' }}
+        >
+          Submit
+        </Button>
+      </div>
+    )}
 
-        {currentQuestionIndex === questions.length && (
-          <div>
-            <Typography variant="h6">Chatbot</Typography>
-            <Typography variant="body1">Thank you for answering the questions.</Typography>
-            <Typography variant="body1">Average sentiment: {averageSentiment}</Typography>
-          </div>
-        )}
-      </Paper>
-    </Container>
+    {/* Chatbot Finished */}
+    {currentQuestionIndex === questions.length && (
+      <div>
+        <Typography variant="h6">Chatbot</Typography>
+        <Typography variant="body1">Thank you for answering the questions.</Typography>
+        <Typography variant="body1">Average sentiment: {averageSentiment}</Typography>
+      </div>
+    )}
+  </Paper>
+</Container>
   );
 }
 
